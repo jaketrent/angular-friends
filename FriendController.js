89d9b283323c6a969cdcabffe22f3d74f2d9d46b
jaketrent <trent.jake@gmail.com>
2014-04-14 21:08:47 -0600

    day 1

--- FriendController.js
+++ FriendController.js
@@ -1,4 +1,13 @@
+angular.module('app').controller('FriendController', function ($scope) {
 
+  $.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json', function (data, status, xhr) {
+    $scope.$apply(function () {
+      $scope.friends = data.results
+    })
+  })
+
+  $scope.test = "Hello World"
+})
 
 
 
